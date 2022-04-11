92fa561058d21eea53fae522747c40032ab94f70
jonathan arsola <jonathan.arsola01@utrgv.edu>
2022-04-10 19:27:41 -0500

    add basic structure to website. skeleton of home and about page done.

---
+++ src/components/Picture.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Picture = () => {
+  return <h1>picture component</h1>
+}
+
+export default Picture
---
+++ src/components/Sidebar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return <h1>Sidebar Component</h1>
+}
+
+export default Sidebar
