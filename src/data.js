f09555ea656fcd8179a1f85806b9a330bb521490
arsoljon <jonathan.arsola01@utrgv.edu>
2022-04-15 04:08:48 -0500

    Update data.js
    fixed typos and added a "tools" key to the projects list.  tools represents additional languages or apps used.

--- src/data.js
+++ src/data.js
@@ -7,13 +7,15 @@ export const projects = [
     picture: 'https://i.imgur.com/3wghHhY.png',
     url: 'https://github.com/arsoljon/Senior-Project-4390',
     desc: 'This is the capstone project for my senior project course. It is a 2d adventure game where a character can travel to different islands, explore underwater, and kill different enemies. The original idea was to have everything underwater with the islands being encapsulated by bubbles. This would result in the player experiencing unique game physics. ',
+    tools: ['Unity', 'C#'],
   },
   {
     id: 2,
     title: 'Stock Picker - Backend',
     picture: 'https://i.imgur.com/7Notqvy.png',
     url: 'https://github.com/arsoljon/StockPickerTTH12-1-Back',
-    desc: 'This is the bachkend portion of a Stock Picker application. Using NodeJS, we communicated to a RESTful API and implemented CRUD operations. Our team organized tasks using the SCRUM methodology. We used Jira to track product development. Lastly, we combined our work with the frontend team at the end of our internship.',
+    desc: 'This is the backend portion of a Stock Picker application. Using NodeJS, we communicated to a RESTful API and implemented CRUD operations. Our team organized tasks using the SCRUM methodology. We used Jira to track product development. Lastly, we combined our work with the frontend team at the end of our internship.',
+    tools: ['NodeJS', 'Jira'],
   },
   {
     id: 3,
@@ -22,13 +24,15 @@ export const projects = [
       'https://media.istockphoto.com/photos/soccer-ball-isolated-picture-id1168750743?k=6&m=1168750743&s=170667a&w=0&h=MHNULzoYtrIkWdDwEF13fqhu1qnNgW64XP_v3swbiqo=',
     url: 'https://github.com/arsoljon/Machine-Learning---Soccer-Stats',
     desc: 'Using machine learning, my team and I created a system that attempted to predict which soccer teams would win the next season. Our data set consisted of 4 years worth of matches from 1 league. We used a support vector machine and a random forest model.',
+    tools: ['Python'],
   },
   {
     id: 4,
     title: 'Neural Network Graph',
     picture: 'https://i.imgur.com/an7DVJ0.png',
     url: 'https://github.com/arsoljon/neural-network-graph',
-    desc: 'This is a visual application that makes a depiction of a nueral network. Instead of tediously creating nodes and lines for each layer, this app allows you to input a number for each layer. Then outputs a picture accordingly.',
+    desc: 'This is a visual application that makes a depiction of a neural network. Instead of tediously creating nodes and lines for each layer, this app allows you to input a number for each layer. Then outputs a picture accordingly.',
+    tools: ['Processing3', 'Python'],
   },
 ]
 
